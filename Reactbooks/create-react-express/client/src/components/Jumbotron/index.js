bd70572669834be8070cfdd5263e0d389f0703e2
Lee Marvin <ryenem8@gmail.com>
2020-11-17 14:07:31 -0800

    added search information

---
+++ Reactbooks/create-react-express/client/src/components/Jumbotron/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "/style.css"
+function Jumbotron() {
+  return (
+        <div className="jumbotron text-center">
+            <div className="container text-light">
+                <h1>Google Books Search</h1>
+                <h4>Search for and Save Books of Interest</h4>
+            </div>
+        </div>
+    );
+}
+export default Jumbotron;
---
+++ Reactbooks/create-react-express/client/src/pages/Search.js
@@ -0,0 +1,37 @@
+// import Bookcard from "../components/Bookcard";
+// import Jumbotron from "../components/Jumbotron";
+// import Navbar from "../components/Navbar";
+// import Wrapper from "../components/Wrapper";
+// import Searchbar from "../components/Searchbar";
+// import { Component } from "react";
+
+// function googleBooksSearch() {  
+//     const [book, setBook] = useState("");  
+//     const [result, setResult] = useState([]);  
+      
+  
+//     function handleChange(event) {  
+//         const book = event.target.value;  
+//         setBook(book);  
+//     }  
+//     function handleSubmit(event) {  
+//         event.preventDefault();  
+//         axios.get("https://www.googleapis.com/books/v1/volumes?q=" + req.body) 
+//             .then(data => {  
+//                 console.log(data.data.items);  
+//                 setResult(data.data.items);  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
