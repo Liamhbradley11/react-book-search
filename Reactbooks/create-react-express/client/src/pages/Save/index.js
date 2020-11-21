5265bdd8e8b04801e028a4f4459a441fe816aca6
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-11-20 17:39:21 -0800

    page renders

--- Reactbooks/create-react-express/client/src/pages/Save/index.js
+++ Reactbooks/create-react-express/client/src/pages/Save/index.js
@@ -1,11 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 import Wrapper from "../../components/Wrapper";
 import Navbar from "../../components/Navbar";
 import { Container } from "../../components/Grid";
 import Bookcard from "../../components/Bookcard";
 import Jumbotron from "../../components/Jumbotron";
-
-import { Component } from "react";
 
 
 class Save extends Component {
