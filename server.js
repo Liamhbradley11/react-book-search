845da03ab5994d98b04207e66ff3fbc540a475d8
ph-kandy <70727213+ph-kandy@users.noreply.github.com>
2020-11-21 15:34:06 -0800

    fix /utils/api and server.js

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 }
 
 // Define API routes here
 
 app.use("/api", apiRoutes);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true })
 
 app.listen(PORT, () => {
