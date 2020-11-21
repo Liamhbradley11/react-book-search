bdd4aefbb752fb0bcf55366ebdd6917227916a5d
ph-kandy <70727213+ph-kandy@users.noreply.github.com>
2020-11-21 13:17:12 -0800

    added in build dependency

---
+++ Reactbooks/create-react-express/client/build/precache-manifest.d0b1cd4bfaff21636acefd514bb4abf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99b32f124d45e76d2941",
+    "url": "/static/css/main.84595336.chunk.css"
+  },
+  {
+    "revision": "99b32f124d45e76d2941",
+    "url": "/static/js/main.7c956e39.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d507ef4c2234aaf623ac",
+    "url": "/static/js/2.aee1101b.chunk.js"
+  },
+  {
+    "revision": "a8b84f60ee2bb89b22c81b750d2085c1",
+    "url": "/index.html"
+  }
+];
