5cb56aba67b8d776b5df8d087330087a4c7e6371
Lee Marvin <ryenem8@gmail.com>
2020-11-21 16:03:27 -0800

    adding saving

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,14 +1,10 @@
 import React from "react";
 // This file exports the Input, TextArea, and FormBtn components
-
-
-
-
 // By extending the React.Component class, Counter inherits functionality from it
-function SearchBar(props, handleSubmit, handleInput){
+function SearchBar(props){
   return (
       <form className="form-group" id="searchform" onSubmit={(event) => props.handleSubmit(event)}>
-        <input className="searchbar" placeholder="Please Enter a Title"onInput={(event) => props.handleInput(event)}/>
+        <input className="searchbar" placeholder="Please Enter a Title" onInput={(event) => props.handleInput(event)}/>
         <button className="btn btn-secondary" type="submit">
           Submit
         </button> 
--- client/src/pages/Save/index.js
+++ client/src/pages/Save/index.js
@@ -1,27 +1,40 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Wrapper from "../../components/Wrapper";
 import Navbar from "../../components/Navbar";
 import { Container } from "../../components/Grid";
 import Bookcard from "../../components/Bookcard";
 import Jumbotron from "../../components/Jumbotron";
-
-
+import Api from "../../utils/Api";
 function Save() {
-
+    const [books, setBooks] = useState ([])
+    function getBook() {
+        Api.getBook() 
+        .then(res => {
+            console.log({res});
+            setBooks(res.data)
+        })
+    }
+    useEffect(getBook, [])
+    console.log({books});
     return (
-
         <Wrapper>
-
             <Navbar />
             <Jumbotron />
             <Container>
-                <Bookcard />
+            {books.map((searchedbooks) => (
+                    <Bookcard
+                        key={searchedbooks._id}  
+                        id={searchedbooks._id}
+                        image={searchedbooks.image}
+                        title={searchedbooks.title}
+                        authors={searchedbooks.authors}
+                        description={searchedbooks.description}
+                        // deleteBook={deleteBook} 
+                        showDelete={true}
+                    />
+                ))}
             </Container>
-
         </Wrapper>
-
     );
 }
-
-
 export default Save;
