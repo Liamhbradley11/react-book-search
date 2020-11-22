05a5f8a1a2a868343c1099f9f27eeac92ebe6fb9
Lee Marvin <ryenem8@gmail.com>
2020-11-21 18:42:22 -0800

    maybe

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
-const apiRoutes = require("routes/api-routes.js");
+const apiRoutes = require("./routes/api-routes.js");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,20 +11,15 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {	
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));	
-  }); 
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 }
-
 // Define API routes here
-
 app.use("/api", apiRoutes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true })
 
-
-
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`:earth_americas: ==> API server now on port ${PORT}!`);
 });
